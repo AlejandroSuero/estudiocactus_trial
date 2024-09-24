fb070b898847b100320d516a646b62230ea69c82
AlejandroSuero <asuerome@gmail.com>
2024-09-25 01:14:00 +0200

    feat: add point to store

--- src/stores/useStore.ts
+++ src/stores/useStore.ts
@@ -1,23 +1,27 @@
 import { create } from "zustand";
-import type { Material } from "~/types/collections";
+import type { Material, Point } from "~/types/collections";
 
 interface MaterialStore {
   isSidebarOpen: boolean;
   selectedMaterial: Material | null;
+  selectedPoint: Point | null;
   toggleSidebar: () => void;
   openSidebar: () => void;
   closeSidebar: () => void;
   selectMaterial: (material: Material) => void;
+  selectPoint: (point: Point) => void;
 }
 
 const useStore = create<MaterialStore>()((set) => ({
   isSidebarOpen: false,
   selectedMaterial: null,
+  selectedPoint: null,
 
   toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
   openSidebar: () => set({ isSidebarOpen: true }),
   closeSidebar: () => set({ isSidebarOpen: false }),
   selectMaterial: (material: Material) => set({ selectedMaterial: material }),
+  selectPoint: (point: Point) => set({ selectedPoint: point }),
 }));
 
 export default useStore;
