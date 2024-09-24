c2d6134bf34134910ba4df57049b9207db563679
AlejandroSuero <asuerome@gmail.com>
2024-09-25 00:05:30 +0200

    feat: add mock data when in development

--- src/lib/getData.ts
+++ src/lib/getData.ts
@@ -1,6 +1,8 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "~/configs/firebase";
 import type { Material, MaterialsByPoint } from "~/types/collections";
+import mockData from "../mocks/mock.json";
+import { env } from "~/env";
 
 /**
  * Fetch data from Firestore
@@ -8,6 +10,18 @@ import type { Material, MaterialsByPoint } from "~/types/collections";
  * @returns An array of objects representing the data from the collection
  */
 export async function getCollection(collectionName: string) {
+  // If in development mode, fetch data from mock data
+  if (env.NODE_ENV === "development") {
+    console.log("Fetching data from Mock Data");
+    if (collectionName === "materials") {
+      return mockData.materials;
+    } else if (collectionName === "points") {
+      return mockData.points;
+    }
+  } else {
+    return null;
+  }
+  // Fetch data from Firestore
   try {
     const querySnapshot = await getDocs(collection(db, collectionName));
     const data = querySnapshot.docs.map((doc) => ({
@@ -17,13 +31,14 @@ export async function getCollection(collectionName: string) {
     return data;
   } catch (error) {
     console.error("Error fetching data from Firestore:", error);
-    return [];
+    return null;
   }
 }
 
-export async function getMaterials(): Promise<MaterialsByPoint> {
+export async function getMaterials(): Promise<MaterialsByPoint | null> {
   const materials = (await getCollection("materials")) as Material[];
   const materialsByPoint: Record<string, Material[]> = {};
+  if (!materials) return null;
   // Group materials by point
   materials.forEach((material) => {
     material.points.forEach((point) => {
