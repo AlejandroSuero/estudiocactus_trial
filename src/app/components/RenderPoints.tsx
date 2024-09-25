51ba17a121ecbb14701b318c9da6f64646fde0c2
AoMe · 青目 <71392160+AlejandroSuero@users.noreply.github.com>
2024-09-25 18:01:22 +0200

    feat: merge dev into main (#6)
    * feat: add `firestore` fetch functionality (#1)
    
    * feat: add `firebase` config
    
    * feat: update firebase config
    
    * feat: add functions to get `firestore` data
    
    * feat: add types for `collections`
    
    * feat: add `images.domains` to next config
    
    * feat: add `react-icons`
    
    * feat: render base image with points
    
    Also added a material on top to check that it works as intended.
    
    * refactor: render at component level
    
    * refactor: `images.domains` is deprecated
    
    * feat: add mock data when in development
    
    * feat: update styles
    
    * feat: add mock data
    
    * feat(deps): add `sharp` for better img optimization
    
    * fix: use `useEffect`, `useMemo`, `memo` and `useCallback`
    
    With this changes it prevents unnecessary re-renders
    that caused the app to not work on production.
    
    * fix: `lg:` screen height
    
    * feat: update layout metadata
    
    * feat: hide points when selecting materials
    
    * feat: update styles and improve UX
    
    Tracking if the user clicks outside the sidebar to close it.
    
    Tracking the materials that are applied for that specific point.
    
    Modify UI for better mobile and desktop experiences.
    
    * feat: use `aspect-ratio` to keep image and points
    
    * fix: remove unnecessary images

--- src/app/components/RenderPoints.tsx
+++ src/app/components/RenderPoints.tsx
@@ -11,6 +11,7 @@ interface PointsProps {
 function RenderPoints({ points }: PointsProps) {
   const openSidebar = useStore((state) => state.openSidebar);
   const setActivePoint = useStore((state) => state.setActivePoint);
+  const isSidebarOpen = useStore((state) => state.isSidebarOpen);
 
   const handlePointClick = useCallback(
     (point: Point) => {
@@ -21,23 +22,25 @@ function RenderPoints({ points }: PointsProps) {
   );
 
   const renderedPoints = useMemo(() => {
+    if (isSidebarOpen) return null;
     return points.map((point) => (
       <div
-        className="absolute flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-black/30 object-contain hover:bg-black/50 md:h-10 md:w-10 lg:h-14 lg:w-14 xl:h-16 xl:w-16"
+        className="absolute flex h-5 w-5 cursor-pointer items-center justify-center rounded-full bg-black/30 object-contain hover:bg-black/50 md:h-7 md:w-7 lg:h-9 lg:w-9"
         key={point.id}
         style={{
           left: `${point.coordX}%`,
           top: `${point.coordY}%`,
         }}
+        title={point.name}
         onClick={() => handlePointClick(point)}
       >
         <IoFingerPrintSharp
-          className="h-7 w-7 rotate-45 rounded-full border-2 border-white p-1 text-white md:h-8 md:w-8 lg:h-12 lg:w-12 xl:h-14 xl:w-14"
+          className="h-4 w-4 rotate-45 rounded-full border border-white p-0.5 text-white md:h-6 md:w-6 lg:h-8 lg:w-8 lg:border-2 lg:p-1"
           size={35}
         />
       </div>
     ));
-  }, [points, handlePointClick]);
+  }, [points, handlePointClick, isSidebarOpen]);
 
   return <>{renderedPoints}</>;
 }
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,61 +1,142 @@
 "use client";
 import Image from "next/image";
-import { memo, useCallback, useMemo } from "react";
+import { memo, useCallback, useEffect, useMemo, useRef } from "react";
 import useStore from "~/stores/useStore";
-import type { Material } from "~/types/collections";
+import { FaCheck } from "react-icons/fa";
+import { MdKeyboardArrowUp } from "react-icons/md";
+import type { Material, Point } from "~/types/collections";
 
 interface SidebarProps {
   materials: Record<string, Material[]>;
+  points: Point[];
 }
 
-function Sidebar({ materials }: SidebarProps) {
+function Sidebar({ materials, points }: SidebarProps) {
   const isSidebarOpen = useStore((state) => state.isSidebarOpen);
   const selectMaterial = useStore((state) => state.selectMaterial);
+  const selectedMaterials = useStore((state) => state.selectedMaterials);
   const activePoint = useStore((state) => state.activePoint);
   const closeSidebar = useStore((state) => state.closeSidebar);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const setActivePoint = useStore((state) => state.setActivePoint);
 
   const handleMaterialClick = useCallback(
     (material: Material) => {
       selectMaterial(activePoint?.id ?? "", material);
-      closeSidebar();
     },
-    [activePoint, selectMaterial, closeSidebar]
+    [activePoint, selectMaterial]
   );
+
+  const handleNextPointClick = useCallback(() => {
+    const nextPointIndex = points.findIndex((point) => point.id === activePoint?.id) + 1;
+    if (nextPointIndex < points.length) {
+      setActivePoint(points[nextPointIndex]!);
+    } else {
+      setActivePoint(points[0]!);
+    }
+  }, [activePoint, setActivePoint, points]);
+
+  const handlePreviousPointClick = useCallback(() => {
+    const previousPointIndex = points.findIndex((point) => point.id === activePoint?.id) - 1;
+    if (previousPointIndex >= 0) {
+      setActivePoint(points[previousPointIndex]!);
+    } else {
+      setActivePoint(points[points.length - 1]!);
+    }
+  }, [activePoint, setActivePoint, points]);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
+        closeSidebar();
+      }
+    };
+
+    if (isSidebarOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+    // document.removeEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      // Cleanup event listener on unmount
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isSidebarOpen, closeSidebar]);
 
   const sidebar = useMemo(() => {
     const pointId = activePoint?.id;
-    if (!pointId) return <p>No point selected</p>;
+    if (!pointId) return null;
+    const currentAppliedMaterial = selectedMaterials[pointId];
+
     return (
       <aside
-        className={`fixed left-0 top-0 z-50 h-screen w-full bg-gray-800 text-white transition-all duration-300 ease-in-out hover:text-gray-800 ${isSidebarOpen ? "translate-x-[85%]" : "translate-x-full"}`}
+        ref={sidebarRef}
+        className={`fixed bottom-0 right-0 z-50 mr-0 h-[60vh] w-full transform rounded-t-xl bg-gray-800/85 px-6 py-4 text-white transition-transform duration-300 ease-in-out md:rounded-xl ${isSidebarOpen ? "translate-y-0 md:mr-2 md:translate-x-0" : "translate-y-full md:mr-0 md:translate-x-full"} md:bottom-auto md:right-0 md:h-fit md:w-80 md:translate-y-0`}
       >
-        <button onClick={closeSidebar} className="fixed right-12 top-0 z-50 p-2 focus:outline-none">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+        <button
+          onClick={handlePreviousPointClick}
+          title="Go to previous point of change"
+          className="flex h-8 w-full items-center justify-center rounded-md border border-white p-2 px-4 hover:bg-gray-400 hover:text-gray-900 focus:outline-none"
+        >
+          <MdKeyboardArrowUp size={50} />
         </button>
-        <ul className="flex flex-col space-y-4 p-4">
-          {materials[pointId]?.map((material: Material) => (
-            <li
-              key={material.id}
-              onClick={() => handleMaterialClick(material)}
-              className="relative flex cursor-pointer items-center space-x-2 rounded-md p-2 text-white hover:bg-gray-400 hover:text-gray-900 focus:outline-none"
-            >
-              <Image
-                src={material.materialPreview}
-                alt={material.name}
-                width={48}
-                height={material.name === "Frame White" ? 99 : 48}
-                loading="lazy"
-                className="h-[48px] w-[48px] rounded-md object-cover"
-              />
-              <span className="text-sm font-medium">{material.name}</span>
-            </li>
-          ))}
+        <div className="flex flex-col items-start justify-center py-4">
+          <h2 className="text-lg font-bold">Materials</h2>
+          <h3 className="text-md font-medium text-gray-200">{activePoint?.name}</h3>
+        </div>
+        <ul className="flex flex-col space-y-4 overflow-y-auto pb-4">
+          {materials[pointId]?.map((material: Material) => {
+            let isActive = false;
+            if (currentAppliedMaterial) {
+              isActive = currentAppliedMaterial[0]?.id === material.id;
+            }
+            return (
+              <li
+                key={material.id}
+                onClick={() => handleMaterialClick(material)}
+                className={`group relative flex cursor-pointer items-center space-x-2 rounded-md p-2 text-white ${isActive ? "bg-gray-500" : ""} hover:bg-gray-400 hover:text-gray-900 focus:outline-none`}
+              >
+                <div className="relative overflow-hidden rounded-xl border-[3px] border-white group-hover:border-gray-900">
+                  <Image
+                    src={material.materialPreview}
+                    alt={material.name}
+                    title={material.name}
+                    width={48}
+                    height={material.name === "Frame White" ? 99 : 48}
+                    loading="lazy"
+                    className="h-[60px] w-[60px] object-cover"
+                  />
+                  <div className={`absolute top-0 h-full w-full ${isActive ? "bg-gray-800/25" : "hidden"}`} />
+                  <FaCheck
+                    size={24}
+                    className={`absolute right-[50%] top-[50%] ${isActive ? "" : "hidden"} -translate-y-[50%] translate-x-[50%]`}
+                  />
+                </div>
+                <span className="text-sm font-medium">{material.name}</span>
+              </li>
+            );
+          })}
         </ul>
+        <button
+          onClick={handleNextPointClick}
+          title="Go to next point of change"
+          className="flex h-8 w-full items-center justify-center rounded-md border border-white p-2 px-4 hover:bg-gray-400 hover:text-gray-900 focus:outline-none"
+        >
+          <MdKeyboardArrowUp size={50} className="rotate-180" />
+        </button>
       </aside>
     );
-  }, [materials, activePoint, handleMaterialClick, isSidebarOpen, closeSidebar]);
+  }, [
+    materials,
+    activePoint,
+    handleMaterialClick,
+    isSidebarOpen,
+    selectedMaterials,
+    handleNextPointClick,
+    handlePreviousPointClick,
+  ]);
 
   return <>{sidebar}</>;
 }
